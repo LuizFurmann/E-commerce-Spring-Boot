826ad3a884d330b1f559156ae1b5b13f24bf7112
Luiz Furmann <furmannluiz@gmail.com>
2024-03-08 21:37:20 -0300

    first commit

---
+++ src/main/java/com/furmannsoft/Fullstack_Seed/FullstackSeedApplication.java
@@ -0,0 +1,13 @@
+package com.furmannsoft.Fullstack_Seed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackSeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/furmannsoft/Fullstack_Seed/FullstackSeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furmannsoft.Fullstack_Seed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackSeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
